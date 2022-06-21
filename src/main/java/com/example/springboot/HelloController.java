7bd3632fc1e91660e8a6b12f414cfed800283cf5
nguyenle2412 <nle8144@gmail.com>
2022-06-21 16:30:52 +0700

    upload project

---
+++ src/main/java/com/example/springboot/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.springboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/")
+	public String index() {
+		return "Hello world";
+	}
+	@GetMapping("/hello")
+	public String say() {
+		return "Hello";
+	}
+
+}
